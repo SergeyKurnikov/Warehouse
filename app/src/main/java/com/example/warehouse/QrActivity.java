a24066ebc510d4c5ee982d8164aad2a19eb28130
shein <kur.ser@mail.ru>
2021-04-25 19:00:32 +0300

    7) Added Zxing library and scaner codes

--- app/src/main/java/com/example/warehouse/QrActivity.java
+++ app/src/main/java/com/example/warehouse/QrActivity.java
@@ -1,18 +1,36 @@
 package com.example.warehouse;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class QrActivity extends AppCompatActivity {
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class QrActivity extends AppCompatActivity implements View.OnClickListener {
+    Button scanBtn;
+    TextView tvScanContent, tvScanFormat;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
+        scanBtn = findViewById(R.id.btnScan);
+        tvScanContent = findViewById(R.id.tvScanContent);
+        tvScanFormat = findViewById(R.id.tvScanFormat);
+        scanBtn.setOnClickListener(this);
     }
     /**
      * Метод, который берёт данные из ресурсов меню и
@@ -37,5 +55,29 @@ public class QrActivity extends AppCompatActivity {
     public void onAdminMenuClick(MenuItem item) {
         Intent intent = new Intent(QrActivity.this, AdminActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setPrompt("Наведите прямоугольник на QR-код");
+        integrator.setOrientationLocked(false);
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null){
+                Toast.makeText(getBaseContext(), "Отменено", Toast.LENGTH_LONG).show();
+            } else{
+                tvScanFormat.setText(result.getFormatName());
+                tvScanContent.setText(result.getContents());
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
