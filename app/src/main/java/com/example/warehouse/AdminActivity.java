ea0ecde3a2a36f1eef84c45ed84c937c88920094
shein <kur.ser@mail.ru>
2021-04-25 22:14:59 +0300

    8) Added logic CRUD and filters

--- app/src/main/java/com/example/warehouse/AdminActivity.java
+++ app/src/main/java/com/example/warehouse/AdminActivity.java
@@ -2,17 +2,68 @@ package com.example.warehouse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class AdminActivity extends AppCompatActivity {
+public class AdminActivity extends AppCompatActivity implements View.OnClickListener {
+    /**Экземпляры класса EditText для полей имени и емайла*/
+    EditText edtId, edtName, edtType, edtNumber, edtProvider, edtDate;
 
+    /**Экземпляры класса Button для кнопок чтения добавления и удаления*/
+    Button btnRead, btnAdd, btnClear, btnUpdate, btnDelete, btnClearOutput;
+
+    /**Экземпляры класса TextView для поля вывода текстовых данных*/
+    TextView tvAllData;
+
+    /**Экземпляры класса DBHelper для взаимодействия с базой данных*/
+    DBHelper dbHelper;
+
+    /**
+     *  Начальная установка объектов при инициализации главной Activity
+     *  @param savedInstanceState - объект класса Bundle для хранения состояния приложения
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        //Инциализация текстовых полей
+        edtId = findViewById(R.id.edtId);
+        edtName = findViewById(R.id.edtName);
+        edtType = findViewById(R.id.edtType);
+        edtNumber = findViewById(R.id.edtNumber);
+        edtProvider = findViewById(R.id.edtProvider);
+        edtDate = findViewById(R.id.edtDate);
+        tvAllData = findViewById(R.id.tvDataAll);
+
+        //Инциализация кнопок
+        btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(this);
+        btnRead = findViewById(R.id.btnRead);
+        btnRead.setOnClickListener(this);
+        btnClear = findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(this);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnUpdate.setOnClickListener(this);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnDelete.setOnClickListener(this);
+        btnClearOutput = findViewById(R.id.btnClearOutput);
+        btnClearOutput.setOnClickListener(this);
+
+        //Инициализация экземплра класса DBHelper
+        dbHelper = new DBHelper(this);
     }
 
     /**
@@ -25,6 +76,7 @@ public class AdminActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_admin, menu);
         return true;
     }
+
     /**
      * Получение идентификаторов выбранного пункта меню через разные обработчики,
      * переход в другие Activity
@@ -38,5 +90,124 @@ public class AdminActivity extends AppCompatActivity {
     public void onQrMenuClick(MenuItem item) {
         Intent intent = new Intent(AdminActivity.this, QrActivity.class);
         startActivity(intent);
+    }
+
+
+    /**
+     * Общий обработчик, который отлавливает нажатия на различные кнопки
+     * @param v - объект класса View для хранения состояния приложения
+     */
+    @Override
+    public void onClick(View v) {
+        String _id = edtId.getText().toString();
+        String name = edtName.getText().toString();
+        String type = edtType.getText().toString();
+        String number = edtNumber.getText().toString();
+        String provider = edtProvider.getText().toString();
+        String date = edtDate.getText().toString();
+
+        // Создание экземпляра класса DSQLiteDatabase
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        String allLines = "";
+        // Наполнение обработчика кнопок
+        switch (v.getId()) {
+            // Логика кнопки добавить
+            case R.id.btnAdd:
+                contentValues.put(DBHelper.KEY_ID, _id);
+                contentValues.put(DBHelper.KEY_NAME, name);
+                contentValues.put(DBHelper.KEY_TYPE, type);
+                contentValues.put(DBHelper.KEY_NUMBER, number);
+                contentValues.put(DBHelper.KEY_PROVIDER, provider);
+                contentValues.put(DBHelper.KEY_DATE, date);
+
+                Log.d("LOG_TAG", "Метод проверки вернул" + checkUserExists(_id, name));
+                if (checkUserExists(_id, name)) {
+                   Toast toast = Toast.makeText(getBaseContext(), "Дублирование позиции!", Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                    tvAllData.setText("Такая позиция уже есть!");
+                    break;
+                }
+                database.insert(DBHelper.TABLE_COMPLETE, null, contentValues);
+                // Логика кнопки прочитать
+            case R.id.btnRead:
+                Cursor cursor = database.query(DBHelper.TABLE_COMPLETE, null, null, null, null, null, null);
+                if (cursor.moveToFirst()) {
+                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
+                    int typeIndex = cursor.getColumnIndex(DBHelper.KEY_TYPE);
+                    int numberIndex = cursor.getColumnIndex(DBHelper.KEY_NUMBER);
+                    int providerIndex = cursor.getColumnIndex(DBHelper.KEY_PROVIDER);
+                    int dateIndex = cursor.getColumnIndex(DBHelper.KEY_DATE);
+                    // Перебирание и вывод строк таблицы
+                    do {
+                        Log.d("myLog", "id = " + cursor.getInt(idIndex) +
+                                ", наименование = " + cursor.getString(nameIndex) +
+                                ", тип = " + cursor.getString(typeIndex) +
+                                ", количество = " + cursor.getString(numberIndex) +
+                                ", поставщик = " + cursor.getString(providerIndex) +
+                                ", дата = " + cursor.getString(dateIndex));
+                        allLines += cursor.getInt(idIndex) + ") "
+                                 + cursor.getString(nameIndex) + " "
+                                 + cursor.getString(typeIndex) + " "
+                                 + cursor.getString(numberIndex) + "шт. "
+                                 + cursor.getString(providerIndex) + " "
+                                 + cursor.getString(dateIndex) + "\n";
+                    } while (cursor.moveToNext());
+                    tvAllData.setText(allLines);
+                }
+                cursor.close();
+                break;
+            // Логика кнопки очистить
+            case R.id.btnClear:
+                database.delete(DBHelper.TABLE_COMPLETE, null, null);
+                break;
+            // Логика кнопки обновить
+            case R.id.btnUpdate:
+                if (_id.equalsIgnoreCase("")) {
+                    break;
+                }
+                contentValues.put(DBHelper.KEY_ID, _id);
+                contentValues.put(DBHelper.KEY_NAME, name);
+                contentValues.put(DBHelper.KEY_TYPE, type);
+                contentValues.put(DBHelper.KEY_NUMBER, number);
+                contentValues.put(DBHelper.KEY_PROVIDER, provider);
+                contentValues.put(DBHelper.KEY_DATE, date);
+                database.update(DBHelper.TABLE_COMPLETE, contentValues,
+                        DBHelper.KEY_ID + " = " + _id, null);
+                break;
+
+            // Логика кнопки удалить
+            case R.id.btnDelete:
+                if (_id.equalsIgnoreCase("")) {
+                    break;
+                }
+                int delCount = database.delete(DBHelper.TABLE_COMPLETE,
+                        DBHelper.KEY_ID + "= ?", new String[]{_id});
+                Log.d("LOG_TAG", "удалено строк = " + delCount);
+                break;
+            // Логика кнопки очистить вывод
+            case R.id.btnClearOutput:
+                tvAllData.setText("");
+                break;
+        }
+        dbHelper.hashCode();
+    }
+
+    /**
+     * Метод валидации новой комплектующей при добавлении по двум признакам
+     * @param _id - id комплектубщей;
+     * @param name - наименование коомплектующей;
+     */
+    public boolean checkUserExists(String _id, String name) {
+        boolean flag;
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        Cursor cursor;
+        String SQLQuery = String.format("SELECT * FROM complete WHERE _id='%s' OR name='%s'", _id, name);
+        cursor = database.rawQuery(SQLQuery, null);
+        flag = cursor.getCount() > 0;
+        cursor.close();
+        return flag;
     }
 }
--- app/src/main/java/com/example/warehouse/MainActivity.java
+++ app/src/main/java/com/example/warehouse/MainActivity.java
@@ -2,17 +2,54 @@ package com.example.warehouse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    /**Экземпляры класса EditText для полей имени и емайла*/
+    EditText edtId, edtName, edtType, edtProvider;
+
+    /**Экземпляры класса Button для кнопок чтения добавления и удаления*/
+    Button btnAll, btnByName, btnByType, btnByProvider;
+
+    /**Экземпляры класса TextView для поля вывода текстовых данных*/
+    TextView tvAllData;
+
+    /**Экземпляры класса DBHelper для взаимодействия с базой данных*/
+    DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Инциализация текстовых полей
+        edtName = findViewById(R.id.edtName);
+        edtType = findViewById(R.id.edtType);
+        edtProvider = findViewById(R.id.edtProvider);
+        tvAllData = findViewById(R.id.tvDataAll);
+
+        //Инциализация кнопок
+        btnAll = findViewById(R.id.btnAll);
+        btnAll.setOnClickListener(this);
+        btnByName = findViewById(R.id.btnByName);
+        btnByName.setOnClickListener(this);
+        btnByType = findViewById(R.id.btnByType);
+        btnByType.setOnClickListener(this);
+        btnByProvider = findViewById(R.id.btnByProvider);
+        btnByProvider.setOnClickListener(this);
+
+        //Инициализация экземплра класса DBHelper
+        dbHelper = new DBHelper(this);
     }
 
     /**
@@ -37,5 +74,140 @@ public class MainActivity extends AppCompatActivity {
     public void onQrMenuClick(MenuItem item) {
         Intent intent = new Intent(MainActivity.this, QrActivity.class);
         startActivity(intent);
+    }
+
+    /**
+     * Общий обработчик, который отлавливает нажатия на различные кнопки
+     * @param v - объект класса View для хранения состояния приложения
+     */
+    @Override
+    public void onClick(View v) {
+        String name = edtName.getText().toString();
+        String type = edtType.getText().toString();
+        String provider = edtProvider.getText().toString();
+        // Создание экземпляра класса DSQLiteDatabase
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        String allLines = "";
+        // Наполнение обработчика кнопок
+        switch (v.getId()) {
+            // Логика кнопки "ВСЕ"
+            case R.id.btnAll:
+                Cursor cursor = database.query(DBHelper.TABLE_COMPLETE, null, null, null, null, null, null);
+                if (cursor.moveToFirst()) {
+                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
+                    int typeIndex = cursor.getColumnIndex(DBHelper.KEY_TYPE);
+                    int numberIndex = cursor.getColumnIndex(DBHelper.KEY_NUMBER);
+                    int providerIndex = cursor.getColumnIndex(DBHelper.KEY_PROVIDER);
+                    int dateIndex = cursor.getColumnIndex(DBHelper.KEY_DATE);
+                    // Перебирание и вывод строк таблицы
+                    do {
+                        Log.d("myLog", "id = " + cursor.getInt(idIndex) +
+                                ", наименование = " + cursor.getString(nameIndex) +
+                                ", тип = " + cursor.getString(typeIndex) +
+                                ", количество = " + cursor.getString(numberIndex) +
+                                ", поставщик = " + cursor.getString(providerIndex) +
+                                ", дата = " + cursor.getString(dateIndex));
+                        allLines += cursor.getInt(idIndex) + ") "
+                                + cursor.getString(nameIndex) + " "
+                                + cursor.getString(typeIndex) + " "
+                                + cursor.getString(numberIndex) + "шт. "
+                                + cursor.getString(providerIndex) + " "
+                                + cursor.getString(dateIndex) + "\n";
+                    } while (cursor.moveToNext());
+                    tvAllData.setText(allLines);
+                }
+                cursor.close();
+                break;
+            // Логика кнопки "ПО НАИМЕНОВАНИЮ"
+            case R.id.btnByName:
+                cursor = database.query(DBHelper.TABLE_COMPLETE, new String[] {"_id","name","type", "number", "provider", "date"}, DBHelper.KEY_NAME + " = ?", new String[] {name}, null, null, null);
+                if (cursor.moveToFirst()){
+                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
+                    int typeIndex = cursor.getColumnIndex(DBHelper.KEY_TYPE);
+                    int numberIndex = cursor.getColumnIndex(DBHelper.KEY_NUMBER);
+                    int providerIndex = cursor.getColumnIndex(DBHelper.KEY_PROVIDER);
+                    int dateIndex = cursor.getColumnIndex(DBHelper.KEY_DATE);
+                    // Перебирание и вывод строк таблицы
+                    do {
+                        Log.d("myLog", "id = " + cursor.getInt(idIndex) +
+                                ", наименование = " + cursor.getString(nameIndex) +
+                                ", тип = " + cursor.getString(typeIndex) +
+                                ", количество = " + cursor.getString(numberIndex) +
+                                ", поставщик = " + cursor.getString(providerIndex) +
+                                ", дата = " + cursor.getString(dateIndex));
+                        allLines += cursor.getInt(idIndex) + ") "
+                                + cursor.getString(nameIndex) + " "
+                                + cursor.getString(typeIndex) + " "
+                                + cursor.getString(numberIndex) + "шт. "
+                                + cursor.getString(providerIndex) + " "
+                                + cursor.getString(dateIndex) + "\n";
+                    } while (cursor.moveToNext());
+                    tvAllData.setText(allLines);
+                }
+                cursor.close();
+                break;
+            // Логика кнопки "ПО ТИПУ"
+            case R.id.btnByType:
+                cursor = database.query(DBHelper.TABLE_COMPLETE, new String[] {"_id","name","type", "number", "provider", "date"}, DBHelper.KEY_TYPE + " = ?", new String[] {type}, null, null, null);
+                if (cursor.moveToFirst()){
+                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
+                    int typeIndex = cursor.getColumnIndex(DBHelper.KEY_TYPE);
+                    int numberIndex = cursor.getColumnIndex(DBHelper.KEY_NUMBER);
+                    int providerIndex = cursor.getColumnIndex(DBHelper.KEY_PROVIDER);
+                    int dateIndex = cursor.getColumnIndex(DBHelper.KEY_DATE);
+                    // Перебирание и вывод строк таблицы
+                    do {
+                        Log.d("myLog", "id = " + cursor.getInt(idIndex) +
+                                ", наименование = " + cursor.getString(nameIndex) +
+                                ", тип = " + cursor.getString(typeIndex) +
+                                ", количество = " + cursor.getString(numberIndex) +
+                                ", поставщик = " + cursor.getString(providerIndex) +
+                                ", дата = " + cursor.getString(dateIndex));
+                        allLines += cursor.getInt(idIndex) + ") "
+                                + cursor.getString(nameIndex) + " "
+                                + cursor.getString(typeIndex) + " "
+                                + cursor.getString(numberIndex) + "шт. "
+                                + cursor.getString(providerIndex) + " "
+                                + cursor.getString(dateIndex) + "\n";
+                    } while (cursor.moveToNext());
+                    tvAllData.setText(allLines);
+                }
+                cursor.close();
+                break;
+            // Логика кнопки "ПО ПОСТАВЩИКУ"
+            case R.id.btnByProvider:
+                cursor = database.query(DBHelper.TABLE_COMPLETE, new String[] {"_id","name","type", "number", "provider", "date"}, DBHelper.KEY_PROVIDER + " = ?", new String[] {provider}, null, null, null);
+                if (cursor.moveToFirst()){
+                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
+                    int typeIndex = cursor.getColumnIndex(DBHelper.KEY_TYPE);
+                    int numberIndex = cursor.getColumnIndex(DBHelper.KEY_NUMBER);
+                    int providerIndex = cursor.getColumnIndex(DBHelper.KEY_PROVIDER);
+                    int dateIndex = cursor.getColumnIndex(DBHelper.KEY_DATE);
+                    // Перебирание и вывод строк таблицы
+                    do {
+                        Log.d("myLog", "id = " + cursor.getInt(idIndex) +
+                                ", наименование = " + cursor.getString(nameIndex) +
+                                ", тип = " + cursor.getString(typeIndex) +
+                                ", количество = " + cursor.getString(numberIndex) +
+                                ", поставщик = " + cursor.getString(providerIndex) +
+                                ", дата = " + cursor.getString(dateIndex));
+                        allLines += cursor.getInt(idIndex) + ") "
+                                + cursor.getString(nameIndex) + " "
+                                + cursor.getString(typeIndex) + " "
+                                + cursor.getString(numberIndex) + "шт. "
+                                + cursor.getString(providerIndex) + " "
+                                + cursor.getString(dateIndex) + "\n";
+                    } while (cursor.moveToNext());
+                    tvAllData.setText(allLines);
+                }
+                cursor.close();
+                break;
+        }
+        dbHelper.hashCode();
     }
 }
